5e2281277951f201c4cdc4326fb83e4be28dc9c6
Phillip Rousu <philliprousu@gmail.com>
2020-04-18 16:42:51 -0700

    Add component

---
+++ client/components/AddToDo.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux'
+import { ADD_TODO, addToDo } from '../store/actions.js';
+
+const AddToDo = ({ addToDo }) => {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addToDo({ title: title, description: description});
+  }
+
+
+  return (
+    <div>
+      <p>Add a ToDo</p>
+      <input onChange={e => setTitle(e.target.value)} type="text" placeholder="Title here" />
+      <textarea onChange={e => setDescription(e.target.value)} placeholder="Description here" />
+      <button onClick={e => handleSubmit(e)}>Submit</button>
+    </div>
+  )
+}
+
+// const mapStateToProps = (state /*, ownProps*/) => {
+//   return {
+//     counter: state.counter
+//   }
+// }
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addToDo: toDo => dispatch(addToDo(toDo))
+  }
+}
+
+export default connect(null, mapDispatchToProps)(AddToDo);
