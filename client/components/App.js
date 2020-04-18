8939c702f8ddbbe1f40c81301435326033de0f1f
Phillip Rousu <philliprousu@gmail.com>
2020-04-18 16:43:29 -0700

    navigation done, plus thunk call to mongoDb

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { getAllToDos } from '../store/actions';
+import Nav from './Nav.js';
+import AllToDos from './AllToDos';
+import SignIn from './SignIn';
+import AddToDo from './AddToDo';
 
-const App = () => {
-  return (
-   <div>hi</div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.props.getAllToDos()
+  }
+
+  render () {
+    return (
+      <BrowserRouter>
+        <Nav></Nav>
+        <Switch>
+          <Route exact path='/'>
+              <AllToDos />
+          </Route>`
+          <Route exact path='/addtodo'>
+            <AddToDo />
+          </Route>
+          <Route exact path='/signin'>
+            <SignIn />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    )
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    getAllToDos: () => dispatch(getAllToDos())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(App);
