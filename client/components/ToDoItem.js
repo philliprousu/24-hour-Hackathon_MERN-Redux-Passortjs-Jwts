2cf37dfefb53168c0fc349381b98c039b63319d9
Phillip Rousu <philliprousu@gmail.com>
2020-04-20 15:57:10 -0700

    refactored to dynamic buttons

--- client/components/ToDoItem.js
+++ client/components/ToDoItem.js
@@ -1,20 +1,46 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux'
+import React, { useState, useEffect } from 'react';
 import styles from '../css/todoitem.css';
 
 
-const ToDoItem = ({title, description, id, handleDelete}) => {
+const ToDoItem = ({title, description, id, handleDelete, updateToDo}) => {
   const [editOpen, setEditOpen] = useState(false);
+  const [titleState, setTitle] = useState('');
+  const [descriptionState, setDescription] = useState('');
 
+  useEffect(() => {
+    setTitle(title); setDescription(description);
+  }, []);
+
+  const cancelEdit = () => {
+    setEditOpen(false);
+    setTitle(title);
+    setDescription(description);
+  }
+
+  const update = () => {
+    updateToDo(id, { title: titleState, description: descriptionState });
+    setEditOpen(false);
+  }
 
   return (
     <div className={styles.outerdiv}>
-      <p>Title: {title}</p>
-      <p>Description: {description}</p>
-      <button onClick={e => handleDelete(id) }>Delete</button>
-      <button onClick={e => setEditOpen(true) }>Edit</button>
+      <div>
+        <p>Title:</p>
+        {
+          editOpen ? <input onChange={e => setTitle(e.target.value)} type='text' value={titleState}/> : <p>{titleState}</p>
+        }
+      </div>
+      <div>
+        <p>Description:</p>
+        {
+          editOpen ? <input onChange={e => setDescription(e.target.value)} type='text' value={descriptionState}/> : <p>{descriptionState}</p>
+        }
+      </div>
       {
-        editOpen && <p>adsfadsf</p>
+        editOpen ? <button onClick={e => cancelEdit()}>Cancel Edit</button> : <button onClick={e => handleDelete(id) }>Delete</button>
+      }
+      {
+        editOpen ? <button onClick={update}>Save</button> : <button onClick={e => setEditOpen(true) }>Edit</button>
       }
     </div>
   )
