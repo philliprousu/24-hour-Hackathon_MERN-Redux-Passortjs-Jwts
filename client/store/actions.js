47180cbb8dbd4147568a64ea8877fe30c66a8885
Phillip Rousu <philliprousu@gmail.com>
2020-04-20 12:30:57 -0700

    tweaked actions

--- client/store/actions.js
+++ client/store/actions.js
@@ -5,8 +5,6 @@ export const GETALL_TODO = 'GETALL_TODO';
 export const ERROR = 'ERROR';
 export const SUCCESS = 'SUCCESS';
 
-// export const addToDo = todo => ({ type: ADD_TODO, payload: todo});
-
 export const addToDo = todo => (dispatch, getState) => {
   return new Promise((resolve, reject) => {
     fetch('http://localhost:3000/todos', {
@@ -15,20 +13,47 @@ export const addToDo = todo => (dispatch, getState) => {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(todo),
-      })
-      .then(response => response.json())
-      .then(result => { dispatch({ type: ADD_TODO, payload: result }); resolve()})
-      .catch(e => { dispatch({ type: ERROR, payload: "Something Went Wrong" }); reject(); }) //dispatch to an error);
+    })
+    .then(response => response.json())
+    .then(result => {
+      dispatch({ type: ADD_TODO, payload: result });
+      dispatch({ type: SUCCESS });
+      resolve(); })
+    .catch(e => {
+      dispatch({ type: ERROR, payload: "Something Went Wrong" });
+      reject();
+    })
   })
 }
-
-
-
-
 
 export const getAllToDos = () => (dispatch, getState) => {
   return fetch('http://localhost:3000/todos')
           .then((response) => response.json())
-          .then(results => dispatch( { type: GETALL_TODO, payload: results } ))
-          .catch(e => console.log(e)) //dispatch to an error);
+          .then(results => {
+            dispatch( { type: GETALL_TODO, payload: results } );
+            dispatch({ type: SUCCESS });
+          })
+          .catch(e => dispatch({ type: ERROR, payload: "Something Went Wrong" }))
+}
+
+export const deleteToDo = id => (dispatch, getState) => {
+  console.log(id)
+  return new Promise((resolve, reject) => {
+    fetch(`http://localhost:3000/todos/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then(response => response.json())
+    .then(result => {
+      dispatch({ type: DELETE_TODO, payload: id });
+      dispatch({ type: SUCCESS });
+      resolve(); })
+    .catch(e => {
+      console.log(e)
+      dispatch({ type: ERROR, payload: "Something Went Wrong" });
+      reject();
+    })
+  })
 }
