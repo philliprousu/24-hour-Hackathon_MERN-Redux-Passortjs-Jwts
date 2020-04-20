e88b661df128280064f7287ba39e3052b8e08bc7
Phillip Rousu <philliprousu@gmail.com>
2020-04-20 15:57:30 -0700

    refactored and added update thunk

--- client/store/actions.js
+++ client/store/actions.js
@@ -18,42 +18,58 @@ export const addToDo = todo => (dispatch, getState) => {
     .then(result => {
       dispatch({ type: ADD_TODO, payload: result });
       dispatch({ type: SUCCESS });
-      resolve(); })
+      resolve();
+    })
     .catch(e => {
       dispatch({ type: ERROR, payload: "Something Went Wrong" });
       reject();
     })
   })
-}
+};
 
-export const getAllToDos = () => (dispatch, getState) => {
-  return fetch('http://localhost:3000/todos')
-          .then((response) => response.json())
-          .then(results => {
-            dispatch( { type: GETALL_TODO, payload: results } );
-            dispatch({ type: SUCCESS });
-          })
-          .catch(e => dispatch({ type: ERROR, payload: "Something Went Wrong" }))
-}
+export const getAllToDos = () => (dispatch, getState) => (
+  fetch('http://localhost:3000/todos')
+    .then((response) => response.json())
+    .then(results => {
+      dispatch( { type: GETALL_TODO, payload: results } );
+      dispatch({ type: SUCCESS });
+    })
+    .catch(e => dispatch({ type: ERROR, payload: "Something Went Wrong" }))
+);
 
 export const deleteToDo = id => (dispatch, getState) => {
-  console.log(id)
-  return new Promise((resolve, reject) => {
-    fetch(`http://localhost:3000/todos/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    })
-    .then(response => response.json())
-    .then(result => {
-      dispatch({ type: DELETE_TODO, payload: id });
-      dispatch({ type: SUCCESS });
-      resolve(); })
-    .catch(e => {
-      console.log(e)
-      dispatch({ type: ERROR, payload: "Something Went Wrong" });
-      reject();
-    })
+  fetch(`http://localhost:3000/todos/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    }
   })
-}
+  .then(response => response.json())
+  .then(result => {
+    dispatch({ type: DELETE_TODO, payload: id });
+    dispatch({ type: SUCCESS });
+  })
+  .catch(e => {
+    console.log(e)
+    dispatch({ type: ERROR, payload: "Something Went Wrong" });
+  })
+};
+
+export const updateToDo = (id, updates) => (dispatch, getState) => {
+  return fetch(`http://localhost:3000/todos/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updates),
+  })
+  .then(response => response.json())
+  .then(result => {
+    dispatch({ type: UPDATE_TODO, payload: result });
+    dispatch({ type: SUCCESS });
+  })
+  .catch(e => {
+    console.log(e)
+    dispatch({ type: ERROR, payload: "Something Went Wrong" });
+  })
+};
