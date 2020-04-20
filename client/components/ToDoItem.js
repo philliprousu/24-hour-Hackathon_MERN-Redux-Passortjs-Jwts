6d1f19bbeed215cec4315e1f08984c81693622a4
Phillip Rousu <philliprousu@gmail.com>
2020-04-20 12:30:04 -0700

    setup editing and deleting capability

--- client/components/ToDoItem.js
+++ client/components/ToDoItem.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux'
+import styles from '../css/todoitem.css';
 
-const ToDoItem = ({title, description}) => {
+
+const ToDoItem = ({title, description, id, handleDelete}) => {
+  const [editOpen, setEditOpen] = useState(false);
+
+
   return (
-    <div>
-      <p>{title}</p>
-      <p>{description}</p>
+    <div className={styles.outerdiv}>
+      <p>Title: {title}</p>
+      <p>Description: {description}</p>
+      <button onClick={e => handleDelete(id) }>Delete</button>
+      <button onClick={e => setEditOpen(true) }>Edit</button>
+      {
+        editOpen && <p>adsfadsf</p>
+      }
     </div>
   )
 }
+
+
 
 export default ToDoItem;
