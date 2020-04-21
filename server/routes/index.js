b5d1357c4ae5463272cd108bb347c35420e084c4
Phillip Rousu <philliprousu@gmail.com>
2020-04-21 15:07:21 -0700

    took care of react-router issue

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ const { addToDo, getAllToDos, editToDo, getOneToDo, deleteToDo } = require('../c
 const { signUp, login } = require('../controllers/auth');
 const passportService = require('../services/passport.js');
 const passport = require('passport');
+const path = require('path')
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireLogin = passport.authenticate('local', { session: false });
@@ -12,33 +13,43 @@ module.exports = app => {
 
   app.post('/signup', signUp);
 
-  app.get('/todos', (req, res) => {
-    getAllToDos()
+  app.get('/todos', requireAuth, (req, res) => {
+    getAllToDos(req.user.id)
       .then(results => res.json(results))
       .catch(e => res.sendStatus(500))
   });
 
-  app.get('/todos/:id', (req, res) => {
+  app.get('/todos/:id', requireAuth, (req, res) => {
     getOneToDo(req.params.id)
       .then(results => res.json(results))
       .catch(e => res.sendStatus(500))
   });
 
-  app.put('/todos/:id', (req, res) => {
+  app.put('/todos/:id', requireAuth, (req, res) => {
     editToDo(req.params.id, req.body)
       .then(results => res.json(results))
       .catch(e => res.sendStatus(500));
   });
 
-  app.post('/todos', (req, res) => {
-    addToDo(req.body)
+  app.post('/todos', requireAuth, (req, res) => {
+    console.log(req.user)
+    addToDo({...req.body, userId: req.user.id })
       .then(results => res.json(results))
       .catch(e => res.sendStatus(500));
   });
 
-  app.delete('/todos/:id', (req, res) => {
+  app.delete('/todos/:id', requireAuth, (req, res) => {
     deleteToDo(req.params.id)
       .then(results => res.json(results))
       .catch(e => res.sendStatus(500));
   });
+
+  app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../../public/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
+
 }
