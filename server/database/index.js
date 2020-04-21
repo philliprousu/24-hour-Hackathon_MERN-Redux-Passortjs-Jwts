f729f766c92ec082350d77c1da5c30833597750b
Phillip Rousu <philliprousu@gmail.com>
2020-04-20 22:40:21 -0700

    rearranged

--- server/database/index.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => console.log('Mongoose Connected'));
