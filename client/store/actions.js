099cbbd51f2bfb678c6f6e96ccab0cbe25a8b237
Phillip Rousu <philliprousu@gmail.com>
2020-04-21 11:24:50 -0700

    login and signup thunks

--- client/store/actions.js
+++ client/store/actions.js
@@ -4,6 +4,71 @@ export const DELETE_TODO = 'DELETE_TODO';
 export const GETALL_TODO = 'GETALL_TODO';
 export const ERROR = 'ERROR';
 export const SUCCESS = 'SUCCESS';
+export const SIGN_UP = 'SIGN_UP';
+export const LOGIN = 'LOGIN';
+export const AUTH_ERROR = 'AUTH_ERROR';
+
+export const login = userObj => dispatch => {
+  return new Promise((resolve, reject) => {
+    fetch('http://localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userObj),
+    })
+    .then(response => response.json())
+    .then(result => {
+      console.log(result, 'login action .then')
+      //IF EMAIL IN USE
+      // if (result.error) {
+      //   dispatch({ type: AUTH_ERROR, payload: result.error });
+      //   reject('Email is in use');
+      // } else {
+        dispatch({ type: LOGIN, payload: result });
+        localStorage.setItem('token', result.token);
+        resolve();
+      //}
+    })
+    .catch(e => {
+      console.log(e, 'login action .catch')
+      dispatch({ type: AUTH_ERROR, payload: e.error });
+      reject(e.error);
+    })
+  })
+}
+
+export const signUp = userObj => dispatch => {
+  console.log(userObj)
+  return new Promise((resolve, reject) => {
+    fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userObj),
+    })
+    .then(response => response.json())
+    .then(result => {
+      console.log(result, 'signUp action .then')
+      //IF EMAIL IN USE
+      if (result.error) {
+        dispatch({ type: AUTH_ERROR, payload: result.error });
+        reject('Email is in use');
+      } else {
+        console.log(result, 'lllllll;llkkll')
+        dispatch({ type: SIGN_UP, payload: result });
+        localStorage.setItem('token', result.token);
+        resolve();
+      }
+    })
+    .catch(e => {
+      console.log(e, 'signUp action .catch')
+      dispatch({ type: AUTH_ERROR, payload: 'Something went wrong' });
+      reject();
+    })
+  })
+}
 
 export const addToDo = todo => (dispatch, getState) => {
   return new Promise((resolve, reject) => {
