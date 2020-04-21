1f3aada021e4e33ed6ac98b880b094cf83123664
Phillip Rousu <philliprousu@gmail.com>
2020-04-20 22:39:28 -0700

    rearrange into mvc for backend

--- server/index.js
+++ server/index.js
@@ -1,45 +1,19 @@
 const express = require('express');
-const cors = require('cors');
-const db = require('./database')
-const { addToDo, getAllToDos, editToDo, getOneToDo, deleteToDo } = require('./database/controllers');
+const Router = require('./routes');
 const app = express();
 app.use(express.json());
 app.use(express.static(__dirname + '/../public'));
+Router(app);
 
-app.get('/todos', (req, res) => {
-  getAllToDos()
-    .then(results => res.json(results))
-    .catch(e => res.sendStatus(500))
-});
+//db connection
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Mongoose Connected'));
 
-app.get('/todos/:id', (req, res) => {
-  getOneToDo(req.params.id)
-    .then(results => res.json(results))
-    .catch(e => res.sendStatus(500))
-});
+let PORT = process.env.PORT || 3000;
 
-app.put('/todos/:id', (req, res) => {
-  editToDo(req.params.id, req.body)
-    .then(results => res.json(results))
-    .catch(e => res.sendStatus(500));
-});
-
-app.post('/todos', (req, res) => {
-  addToDo(req.body)
-    .then(results => res.json(results))
-    .catch(e => res.sendStatus(500));
-});
-
-app.delete('/todos/:id', (req, res) => {
-  deleteToDo(req.params.id)
-    .then(results => res.json(results))
-    .catch(e => res.sendStatus(500));
-});
-
-let PORT = process.env.PORT;
-if (PORT == null || PORT == "") {
-  PORT = 3000;
-};
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`)
 });
