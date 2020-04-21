0c151c99c495c988fde5cc6f70b05266e5c7d7e4
Phillip Rousu <philliprousu@gmail.com>
2020-04-21 15:06:10 -0700

    refactored reducer

--- client/store/reducers.js
+++ client/store/reducers.js
@@ -1,9 +1,8 @@
-import { ADD_TODO, DELETE_TODO, UPDATE_TODO, GETALL_TODO, ERROR, AUTH_USER, AUTH_ERROR, SUCCESS } from './actions.js';
+import { ADD_TODO, DELETE_TODO, UPDATE_TODO, GETALL_TODO, ERROR, AUTH_USER, AUTH_ERROR, SUCCESS, RESET_TODO } from './actions.js';
 
 const INITIAL_STATE = { auth: '', error: false };
 
 export const authReducer = (state = INITIAL_STATE, action) => {
-  console.log(action)
   switch (action.type) {
     case AUTH_USER:
       return {...state, auth: action.payload.token, error: null}
@@ -17,7 +16,9 @@ export const authReducer = (state = INITIAL_STATE, action) => {
 export const toDoReducer = (state = [], action) => {
   switch (action.type) {
     case GETALL_TODO:
-      return [...state, ...action.payload];
+      return action.payload;
+    case RESET_TODO:
+      return [];
     case ADD_TODO:
       return [...state, action.payload];
     case DELETE_TODO:
