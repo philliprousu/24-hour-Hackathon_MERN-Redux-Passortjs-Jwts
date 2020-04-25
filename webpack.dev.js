bf3eb6f8b297e3ed55ab01a9b7c4c8b9438a6d1c
Phillip Rousu <philliprousu@gmail.com>
2020-04-25 12:57:43 -0700

    optimized for dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,10 +2,18 @@ const path = require('path');
 const config = require('./webpack.config');
 const merge = require('webpack-merge');
 
+// module.exports = merge(config, {
+//   mode: 'development',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.join(__dirname, 'public'),
+//   }
+// });
 module.exports = merge(config, {
   mode: 'development',
+  devtool: 'eval-source-map',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.join(__dirname, 'public'),
   }
 });
