def587e4d0eb57b434fc0701198b1e5d6af8c4a9
Phillip Rousu <philliprousu@gmail.com>
2020-04-25 12:57:59 -0700

    optimized for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const config = require('./webpack.config');
 const merge = require('webpack-merge');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = merge(config, {
   mode: 'production',
+  //advised to have 'source-map' from webpack docs for production
+  //devtool: 'source-map',
   output: {
     filename: 'bundle[contentHash].js',
     path: path.join(__dirname, 'public'),
   },
-  plugins: [new CleanWebpackPlugin()]
 });
